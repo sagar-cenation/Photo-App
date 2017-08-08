6c187b9adb16c6431120238f5ea13ec9e2b6fe59
sagar-cenation <sagarkeshri@rocketmail.com>
2017-08-08 17:49:52 +0530

    add devise and styling

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
-  def index
-  end
+	skip_before_action :authenticate_user!, only: [:index] #skip before action for index action
+	def index
+
+	end
 end
